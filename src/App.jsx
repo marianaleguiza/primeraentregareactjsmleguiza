a30386e3da45359dc82dead7de1ac0c8ce1dc229
marianaleguiza <mariana.leguiza@gmail.com>
2023-09-03 15:54:18 -0300

    segunda preentrega react js mariana leguiza

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import './App.css'
 import NavBar from './Components/NavBar/NavBar'
 import ItemListContainer from './Components/ItemListContainer/ItemListContainer'
 import classes from './App.module.css';
+import ItemCount from './Components/ItemCount/ItemCount.jsx';
 
 function App() {
   return (
     <>
     <div>
     <NavBar />
+    <ItemCount initial={1} stock={30} onAdd={(quantity) => console.log('Cantidad Agregada', quantity)} />
     <ItemListContainer greeting={'Bienvenidos a Freestyle Clothes'} />
     <h2 className={classes.title}>Aloha!🌴
     </h2>
